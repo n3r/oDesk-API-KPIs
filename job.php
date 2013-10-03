82fdd2a3991acf8148b276d9dd787a90e0e71f6a
Nikita Fedorov <nikita@textmagic.biz>
2013-10-03 16:57:51 +0700

    Minor fixes and updates:  - Now it uses bigger interval of dates to fit previous and current week.

--- job.php
+++ job.php
@@ -5,16 +5,23 @@ error_reporting(-1);
 require 'oDeskAPI.lib.php';
 
 function convert($earnings){
+	if ($earnings == 0){
+		return 0;
+	}
     $earnings_dollar = round($earnings, 2);
     $earnings_temp = $earnings_dollar - ceil($earnings_dollar / 300) * 3 - 5;
     $earnings_rub = round(($earnings_temp - $earnings_temp * 0.03) * 32);
-    return $earnings_rub;
+	if ($earnings_rub > 0){
+		return $earnings_rub;
+	}else{
+		return 0;
+	}
 }
 
 $odesk_user = '';
 $odesk_pass = '';
-$secret     = '';
-$api_key    = '';
+$secret     = 'd296e44582e94513';
+$api_key    = 'ee045279a6a66b956ccde55f483a924b';
 
 // Set up variables
 // current week
@@ -32,6 +39,7 @@ $api_key    = '';
 // today
 (float)$t_hours = 0;
 (float)$t_earnings = 0;
+// other
 
 
 
@@ -50,12 +58,18 @@ $url = 'https://www.odesk.com/api/hr/v2/users/me.json';
 $response = $api->get_request($url, array());
 $info = json_decode($response);
 
-// GET THIS MONTH EARNINGS
+// дата начала месяца
 $from = date('Y-m-01',strtotime('this month'));
+// дата конца месяца
 $to = date('Y-m-t',strtotime('this month'));
 
+// GET THIS MONTH EARNINGS
+$start_week = date("W", strtotime($from)) - 1;
+$start = date("Y-m-d", strtotime("2013-W".$start_week."-1"));
+$end = date("Y-m-d", strtotime("2013-W".date("W", strtotime($to))."-7"));
+
 $params = array(
-    'tq' => "SELECT worked_on, assignment_team_id, hours, earnings, earnings_offline, task, memo WHERE worked_on >= '".$from."' AND worked_on <= '".$to."'",
+    'tq' => "SELECT worked_on, assignment_team_id, hours, earnings, earnings_offline, task, memo WHERE worked_on >= '".$start."' AND worked_on <= '".$end."'",
     'tqx' => 'out:json'
 );
 
@@ -63,56 +77,72 @@ $url = "https://www.odesk.com/gds/timereports/v1/providers/".$info->user->id;
 $response = $api->get_request($url, $params);
 $cm = $data = json_decode($response);
 
-$result = array();
+$result = array(
+	(int)date("m") => array(
+		(int)date("d") => array(
+            "hours" => 0,
+            "earnings" => 0
+		)
+	)
+);
 foreach ($data->table->rows as $row){
-    $cm_hours += $row->c[2]->v;
-    $cm_earnings += $row->c[3]->v;
+    $month = (int)substr($row->c[0]->v, 4, 2);
     $day = (int)substr($row->c[0]->v, -2);
-    if (empty($result[$day])){
-        $result[$day] = array(
+	
+	$m = (int)date('m');
+	if ($month == $m){
+		$cm_hours += $row->c[2]->v;
+		$cm_earnings += $row->c[3]->v;
+	}
+    if (empty($result[$month][$day])){
+        $result[$month][$day] = array(
             "hours" => $row->c[2]->v,
             "earnings" => $row->c[3]->v
         );
     }else{
-        $result[$day]["hours"] += $row->c[2]->v;
-        $result[$day]["earnings"] += $row->c[3]->v;
-    }
-    
-    if ($row->c[1]->v == "textmagic:phpsql"){
-        $result[$day]['tm_hours'] += $row->c[2]->v;
-    }
-    if ($row->c[1]->v == "bettybaxter"){
-        $result[$day]['bb_hours'] += $row->c[2]->v;
+        $result[$month][$day]["hours"] += $row->c[2]->v;
+        $result[$month][$day]["earnings"] += $row->c[3]->v;
     }
 
 }
 ksort($result);
+foreach($result as $m){
+	ksort($m);
+}
+
 // This week earnings:
-$start = date("j", strtotime("2013-W".date("W")."-1"));
-$end = date("j", strtotime("2013-W".date("W")."-7"));
-for ($i=$start;$i<=$end;$i++){
-    if (!empty($result[$i])){
-        $cw_earnings += $result[$i]['earnings'];
-        $cw_hours += $result[$i]['hours'];
+$start = (int)strtotime("2013-W".date("W")."-1");
+$end = (int)strtotime("2013-W".date("W")."-7");
+$k = 86400;
+for ($i=$start;$i<=$end;$i=$i+$k){
+	$m = (int)date("m", $i);
+	$d = (int)date("d", $i);
+	
+    if (!empty($result[$m][$d])){
+        $cw_earnings += $result[$m][$d]['earnings'];
+        $cw_hours += $result[$m][$d]['hours'];
     }
 }
 $cw_earnings_rub = convert($cw_earnings);
 
 // Last week earnings
-$start = date("j", strtotime("-1 week", strtotime("2013-W".date("W")."-1")));
-$end = date("j", strtotime("-1 week", strtotime("2013-W".date("W")."-7")));
-for ($i=$start;$i<=$end;$i++){
-    if (isset($result[$i])){
-        $lw_earnings += $result[$i]['earnings'];
-        $lw_hours += $result[$i]['hours'];
+$start = strtotime(date("Y-m-d", strtotime("-1 week", strtotime("2013-W".date("W")."-1"))));
+$end = strtotime(date("Y-m-d", strtotime("-1 week", strtotime("2013-W".date("W")."-7"))));
+$k = 86400;
+for ($i=$start;$i<=$end;$i=$i+$k){
+	$m = (int)date("m", $i);
+	$d = (int)date("d", $i);
+	
+    if (!empty($result[$m][$d])){
+        $lw_earnings += $result[$m][$d]['earnings'];
+        $lw_hours += $result[$m][$d]['hours'];
     }
 }
 $lw_earnings_rub = convert($lw_earnings);
 
-
 // Today's earnings
-$t_earnings_rub = convert($result[date('d')]['earnings']);
-$t_hours = $result[date('d')]['hours'];
+$t_earnings_rub = convert($result[date("m")][(int)date('d')]['earnings']);
+$t_hours = $result[date('m')][(int)date('d')]['hours'];
 
 // Current month earnings
 $cm_earnings_rub = convert($cm_earnings);
